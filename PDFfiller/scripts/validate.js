5a4c8e021377c59687b08795f270f6b58136550a
galayder <lego904@yandex.ru>
2017-05-15 15:42:22 +0300

    Add files via upload

--- PDFfiller/scripts/validate.js
+++ PDFfiller/scripts/validate.js
@@ -1,76 +1,71 @@
-$().ready(function() {
+$("#checkout").validate({
 
-  $('input').on("keypress", function(e) {
-    /* ENTER PRESSED*/
-    if (e.keyCode == 13) {
-      /* FOCUS ELEMENT */
-      var inputs = $(this).parents("form").eq(0).find(":input");
-      var idx = inputs.index(this);
+  submitHandler: function(form) {
+    // do other things for a valid form
+    form.submit();
+  },
 
-      if (idx == inputs.length - 1) {
-        inputs[0].select()
-      } else {
-        inputs[idx + 1].focus(); //  handles submit buttons
-        inputs[idx + 1].select();
-      }
-      return false;
-    }
-  });
-
-  $('#checkout').validate({
-
-    invalidHandler: function(event, validator) {
-      // 'this' refers to the form
-      form.submit();
-      var errors = validator.numberOfInvalids();
-      if (errors) {
-        var message = errors == 1 ?
-          'You missed 1 field. It has been highlighted' :
-          'You missed ' + errors + ' fields. They have been highlighted';
-        $("div.error span").html(message);
-        $("div.error").show();
-      } else {
-        $("div.error").hide();
-      }
+  rules: {
+    firstname: {
+      required: true,
+      minlength: 2,
+      maxlength: 20
     },
-
-    rules: {
-      focusInvalid: false,
-      cardnumber: {
-        required: true,
-        creditcard: true,
-        minlength: 16
-      },
-      cvv: {
-        required: true,
-        minlength: 5
-      },
-      firstname: {
-        required: true,
-        minlength: 2,
-        maxlength: 20
-      },
-      lastname: {
-        required: true,
-        minlength: 2,
-        maxlength: 20
-      },
+    lastname: {
+      required: true,
+      minlength: 2,
+      maxlength: 20
     },
-    messages: {
-      cardnumber: {
-        required: 'Provide your card number'
-      },
-      firstname: {
-        required: 'What is your First name?',
-        minlength: 'Too short',
-        maxlength: 'Wow, how do you spell it?)'
-      },
-      lastname: {
-        required: 'What is your Last Name?',
-        minlength: 'Too short',
-        maxlength: 'Wow, how do you spell it?)'
-      }
+    cardnumber: {
+      required: true,
+      creditcard: true,
+      minlength: 16
+    },
+    month: {
+      required: true
+    },
+    year: {
+      required: true
+    },
+    cvv: {
+      required: true,
+      length: 4
+    },
+    zipcode: {
+      required: true,
+      length: 5
     }
-  })
+  },
 
-})
+  messages: {
+    lasname: {
+      required: "Please, fill in",
+      minlength: "Too short",
+      maxlength: "Wow, how do you spell it?)"
+    },
+    lastname: {
+      required: "Please, fill in",
+      minlength: "Too short",
+      maxlength: "Wow, how do you spell it?)"
+    },
+    cardnumber: {
+      required: "Provide creditcard number",
+      creditcard: "It's a creditcard",
+      length: "Must be 16 digits"
+    },
+    month: {
+      required: "Error"
+    },
+    year: {
+      required: "Error"
+    },
+    cvv: {
+      required: "Error",
+      minlength: "Error"
+    },
+    zipcode: {
+      required: "Error"
+    }
+  },
+
+});
---
+++ PDFfiller/success.html
@@ -0,0 +1,1 @@
+<h1>Submition succsessfull!</h1>
