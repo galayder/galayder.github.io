bf39c0ab3c3d7068633718b41c0d9b4844502dc0
Oleh Stepanenko <lego904@yandex.ru>
2017-09-12 12:42:37 +0300

    uploaded resources and public files

---
+++ SuperDeal/makefile.js
@@ -0,0 +1,137 @@
+var gulp         = require('gulp'),
+    css_compile  = require('gulp-sass'),
+    postcss      = require('gulp-postcss'),
+    browserSync  = require('browser-sync'),
+    cssnano      = require('gulp-cssnano'),
+    autoprefixer = require('autoprefixer'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    iconfont     = require("gulp-iconfont"),
+    consolidate  = require("gulp-consolidate"),
+    del          = require('del'),
+    imagemin     = require('gulp-imagemin'),
+    pngquant     = require('imagemin-pngquant'),
+    cache        = require('gulp-cache'),
+    pug          = require('gulp-pug');
+
+gulp.task('css_compile', function(){
+    return gulp.src('src/**/*.sass')
+        .pipe(sourcemaps.init())
+        .pipe(css_compile({}))
+        .pipe(postcss([ autoprefixer({ browsers: ['last 3 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.reload({
+        stream: true
+    }))
+});
+
+
+// Compile pug to html
+gulp.task('pug', function() {
+    return gulp.src('src/**/*.pug')
+        .pipe(pug({
+          pretty: true
+        }))
+        .pipe(gulp.dest('public'));
+});
+
+
+// Build icon font
+gulp.task("build:icons", function() {
+   return gulp.src(["src/assets/font-icons/*.svg"])//path to svg icons
+     .pipe(iconfont({
+       fontName: "myicons",
+       formats: ["ttf", "eot", "woff", "svg"],
+       centerHorizontally: true,
+       fixedWidth: true,
+       normalize: true
+     }))
+     .on("glyphs", function (glyphs) {
+
+       gulp.src("src/assets/font-icons/util/*.scss") // Template for scss files
+           .pipe(consolidate("lodash", {
+               glyphs: glyphs,
+               fontName: "myicons",
+               fontPath: "../fonts/"
+           }))
+           .pipe(gulp.dest("src/sass"));//generated scss files with classes
+     })
+     .pipe(gulp.dest("src/assets/fonts/"));//icon font destination
+});
+
+
+// Start browserSync
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'public'
+        },
+        port: 3000,
+        open: true,
+        notify: true,
+        browser: "chrome.exe"
+    })
+});
+
+
+// Minify css for build
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('public/main.css') // Choose style to minify
+        .pipe(cssnano()) // Minifying
+        .pipe(rename({suffix: '.min'})) // Adding suffix .min
+        .pipe(gulp.dest('public')); // Load to destination
+});
+
+
+// Watching changes
+gulp.task('default', ['watch']);
+gulp.task('watch', ['browserSync', 'css_compile', 'pug'], function(){
+    gulp.watch('src/templates/**/*.pug', ['pug']);
+    gulp.watch('src/**/*.sass', ['css_compile']);
+    gulp.watch('src/**/*.html').on('change', browserSync.reload);
+});
+
+
+// Clear Gulp cache
+gulp.task('clear', function () {
+    return cache.clearAll();
+});
+
+
+// Clear dist before build
+gulp.task('clean', function() {
+    return del.sync('public/');
+});
+
+
+// Optimize images
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*') // Берем все изображения
+        .pipe(cache(imagemin({ // Сжимаем их с наилучшими настройками
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('public/img')); // Выгружаем на продакшен
+});
+
+
+// Build to public
+gulp.task('build', ['img', 'css_compile'], function() {
+
+    var buildCss = gulp.src([ // Put css to production
+        'src/**/*.css'
+        ])
+    .pipe(gulp.dest('public'));
+
+    var buildFonts = gulp.src('src/assets/fonts/**/*') // Put fonts to production
+    .pipe(gulp.dest('public/assets/fonts'));
+
+    var buildJs = gulp.src('src/js/**/*') // Put scripts to production
+    .pipe(gulp.dest('public/js'));
+
+    var buildHtml = gulp.src('src/*.html') // Put html to production
+    .pipe(gulp.dest('public'));
+
+});
